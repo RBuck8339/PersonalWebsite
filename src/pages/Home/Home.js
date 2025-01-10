ad8fa8c06aee45ef6bcff133a9cb38c74b6582ab
RBuck8339 <101837795+RBuck8339@users.noreply.github.com>
2025-01-09 23:55:13 -0500

    Updated homepage styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -13,44 +13,25 @@ export function Home(){
     )
 }
 
-// Needs editing; serves as a base for now
-function oldProfile(){
-    return (
-        <div id="Profile" className={styles.profile}>
-            <div className={styles.intro}>
-                <h1 className={styles.profileName}>Ronan Buck</h1>
-            </div>
-            <div className={styles.body}>
-
-            </div>
-            <div className={styles.about}>
-
-            </div>
-            <div className={styles.profileContent}>
-                <p className={styles.profileText}>
-                    I am a Junior Computer Science major at the University of Central Florida. With a passion for machine learning, and artificial intelligence,
-                    I am currently an undergraduate researcher with a focus on temporal graph neural networks. 
-                </p>
-                <img className={styles.profileImage} href='logo192.png' alt="picture/logo here"/>
-            </div>
-        </div>
-    )
-}
-
 
 function Profile(){
     return (
         <div id="Profile" className={styles.profile}>
             <div className={styles.intro}>
-                <h1 className={styles.profileName}>Hello! I am&nbsp;<span className={styles.Name}> Ronan Buck</span></h1>
+                {/*Later, change the "Software Developer" to be rotating text of my titles, "Researcher", etc. */}
+                <h1 className={styles.profileName}>Hi! I am&nbsp;<span className={styles.Name}> Ronan Buck</span>, a Software Developer</h1>
             </div>
             <div className={styles.profileContent}>
-                <div className={styles.body}>
-                    I am a Junior Computer Science student attending the University of Central Florida
-                </div>
-                <div className={styles.aboutMe}>
-                    Using my skills, I aim to apply machine learning to real world problems, preferably in the fields of robotics or cybersecurity. I am always seeking new opportunities to learn and would love to hear from you.
-                </div>
+            <div className={styles.body}>
+                I am a Junior Computer Science student attending the University of Central Florida, driven by a passion for solving real-world problems through technology. 
+                My dedication to innovation has led me to become an Undergraduate Researcher, where I explore and develop Graph Neural Networks for Ethereum datasets. 
+                I thrive in collaborative environments and enjoy contributing to impactful projects involving robotics and cybersecurity.
+            </div>
+            <div className={styles.aboutMe}>
+                Using my skills, I aim to apply machine learning to challenges that can make a meaningful difference in the world. 
+                I am eager to learn and grow with every project I undertake, constantly seeking opportunities to grow as a developer. 
+                Feel free to reach out for collaboration or to discuss any new opportunities. I would love to hear from you!
+            </div>
             </div>
         </div>
     )
